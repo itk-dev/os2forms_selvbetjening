dfef3bc2e8ff0bdcdf8ab75af674d6cf08b0ba58
Mikkel Ricky <rimi@aarhus.dk>
2022-04-20 14:59:56 +0200

    CHAT-16: Added info on API endpoints

--- web/modules/custom/os2forms_rest_api/src/WebformHelper.php
+++ web/modules/custom/os2forms_rest_api/src/WebformHelper.php
@@ -5,6 +5,8 @@ namespace Drupal\os2forms_rest_api;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+use Drupal\user\Entity\User;
 use Drupal\webform\WebformInterface;
 use Drupal\webform\WebformSubmissionInterface;
 
@@ -55,6 +57,99 @@ class WebformHelper {
       '#description' => $this->t("Limits users allowed to access this form's data via the REST API"),
       '#default_value' => $allowedUsers,
     ];
+
+    $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints'] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('API endpoints'),
+
+      'links' => [],
+
+      'messages' => [
+        '#markup' => $this->t('Share these endpoints with people that must will use the REST API. Authentification is required to access the endpoints.'),
+      ],
+
+    ];
+
+    $routes = [
+      'rest.webform_rest_elements.GET',
+      'rest.webform_rest_fields.GET',
+      'rest.webform_rest_submission.GET',
+    ];
+    $requireUuid = static function ($route) {
+      return in_array(
+        $route,
+        [
+          'rest.webform_rest_submission.GET',
+          'rest.webform_rest_submission.PATCH',
+        ],
+        TRUE
+      );
+    };
+
+    $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints']['links']['#prefix'] = '<ol>';
+    $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints']['links']['#suffix'] = '</ol>';
+
+    foreach ($routes as $route) {
+      $parameters = [];
+
+      if ('rest.webform_rest_submit.POST' !== $route) {
+        $parameters['webform_id'] = $webform->id();
+      }
+      $uuidPlaceholder = '{uuid}';
+      if ($requireUuid($route)) {
+        $parameters['uuid'] = $uuidPlaceholder;
+      }
+
+      $url = Url::fromRoute($route, $parameters, ['absolute' => TRUE]);
+      $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints']['links'][$route] = [
+        '#type' => 'link',
+        '#title' => str_replace(urlencode($uuidPlaceholder), $uuidPlaceholder, $url->toString()),
+        '#url' => $url,
+        '#prefix' => '<li>',
+        '#suffix' => '</li>',
+      ];
+    }
+
+    $user = User::load(\Drupal::currentUser()->id());
+    $apiKey = $user->isAuthenticated() ? ($user->api_key->value ?? NULL) : NULL;
+    if (NULL !== $apiKey) {
+      $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints_test'] = [
+        '#type' => 'fieldset',
+        '#title' => $this->t('Test API endpoints'),
+
+        'links' => [],
+
+        'message' => [
+          '#markup' => $this->t('These are only for checking the API responses. <strong>Do not</strong> share these endpoints!'),
+        ],
+      ];
+
+      $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints_test']['links']['#prefix'] = '<ol>';
+      $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints_test']['links']['#suffix'] = '</ol>';
+
+      foreach ($routes as $route) {
+        $parameters = [];
+
+        if ('rest.webform_rest_submit.POST' !== $route) {
+          $parameters['webform_id'] = $webform->id();
+        }
+        $uuidPlaceholder = '{uuid}';
+        if ($requireUuid($route)) {
+          $parameters['uuid'] = $uuidPlaceholder;
+        }
+        $parameters['api-key'] = $apiKey;
+
+        $url = Url::fromRoute($route, $parameters, ['absolute' => TRUE]);
+        $form['third_party_settings']['os2forms']['os2forms_rest_api']['api_info']['endpoints_test']['links'][$route] = [
+          '#type' => 'link',
+          '#title' => str_replace(urlencode($uuidPlaceholder), $uuidPlaceholder, $url->toString()),
+          '#url' => $url,
+          '#prefix' => '<li>',
+          '#suffix' => '</li>',
+        ];
+      }
+    }
+
   }
 
   /**
