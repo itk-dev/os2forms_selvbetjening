ab31df8387d1ce1bc98e3d708753332c09f0d73c
Mikkel Ricky <rimi@aarhus.dk>
2022-05-09 17:33:43 +0200

    DW-535: Added API request handler

---
+++ web/modules/custom/os2forms_api_request_handler/src/Plugin/WebformHandler/WebformHandler.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace Drupal\os2forms_api_request_handler\Plugin\WebformHandler;
+
+use Drupal\advancedqueue\Entity\Queue;
+use Drupal\advancedqueue\Job;
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Render\RendererInterface;
+use Drupal\os2forms_api_request_handler\Plugin\AdvancedQueue\JobType\PostSubmission;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Drupal\webform\WebformSubmissionConditionsValidatorInterface;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform\WebformTokenManagerInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * API webform handler.
+ *
+ * @WebformHandler(
+ *   id = "os2forms_api_request_handler",
+ *   label = @Translation("API request handler"),
+ *   category = @Translation("Web services"),
+ *   description = @Translation("Send submission to an API endpoint."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_UNLIMITED,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_IGNORED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_REQUIRED,
+ * )
+ */
+class WebformHandler extends WebformHandlerBase {
+  /**
+   * The queue id.
+   *
+   * @var string
+   */
+  private string $queueId = 'os2forms_api_request_handler';
+
+  /**
+   * Constructor.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, LoggerChannelFactoryInterface $loggerFactory, ConfigFactoryInterface $configFactory, RendererInterface $renderer, EntityTypeManagerInterface $entityTypeManager, WebformSubmissionConditionsValidatorInterface $conditionsValidator, WebformTokenManagerInterface $tokenManager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->setConfiguration($configuration);
+    $this->loggerFactory = $loggerFactory;
+    $this->configFactory = $configFactory;
+    $this->renderer = $renderer;
+    $this->entityTypeManager = $entityTypeManager;
+    $this->conditionsValidator = $conditionsValidator;
+    $this->tokenManager = $tokenManager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('logger.factory'),
+      $container->get('config.factory'),
+      $container->get('renderer'),
+      $container->get('entity_type.manager'),
+      $container->get('webform_submission.conditions_validator'),
+      $container->get('webform.token_manager')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state) {
+    if (NULL === $this->getQueue()) {
+      $form['queue_message'] = [
+        '#theme' => 'status_messages',
+        '#message_list' => [
+          'warning' => [$this->t('Cannot get queue @queue_id', ['@queue_id' => $this->queueId])],
+        ],
+      ];
+    }
+
+    $form['api_url'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API url'),
+      '#description' => $this->t('The API url. For testing, an api url can be obtained from <a href="https://webhook.site">Webhook.site</a>.'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration['api_url'] ?? '',
+    ];
+
+    $form['api_token'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API token'),
+      '#description' => $this->t('The API token. Will be sent in an authorization header: <code>Authorization: Token «API token»</code>.'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration['api_token'] ?? '',
+    ];
+
+    return $this->setSettingsParents($form);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state) {
+    parent::submitConfigurationForm($form, $form_state);
+    $this->configuration['api_url'] = $form_state->getValue('api_url');
+    $this->configuration['api_token'] = $form_state->getValue('api_token');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function postSave(WebformSubmissionInterface $submission, $update = TRUE) {
+    $queue = $this->getQueue();
+    if (NULL === $queue) {
+      $this->loggerFactory->get('os2forms_api_request_handler')->error(sprintf('Cannot get %s queue', $this->queueId));
+      return;
+    }
+
+    $job = Job::create(PostSubmission::class, [
+      'submission' => [
+        'id' => $submission->id(),
+      ],
+      'handler' => [
+        'configuration' => $this->configuration,
+      ],
+    ]);
+    $queue->enqueueJob($job);
+  }
+
+  /**
+   * Get queue.
+   */
+  private function getQueue(): ?Queue {
+    $queueStorage = $this->entityTypeManager->getStorage('advancedqueue_queue');
+    /** @var ?\Drupal\advancedqueue\Entity\Queue $queue */
+    $queue = $queueStorage->load($this->queueId);
+
+    return $queue;
+  }
+
+}
---
+++ web/modules/custom/os2forms_api_request_handler/src/PostHelper.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Drupal\os2forms_api_request_handler;
+
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Language\LanguageInterface;
+use Drupal\Core\Language\LanguageManagerInterface;
+use Drupal\Core\Url;
+use Drupal\os2forms_api_request_handler\Exception\ApiHandlerException;
+use Drupal\webform\WebformSubmissionInterface;
+use GuzzleHttp\Client;
+use GuzzleHttp\RequestOptions;
+
+/**
+ * The POST helper.
+ */
+class PostHelper {
+
+  /**
+   * The EntityTypeManagerInterface.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  private EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The language manager.
+   *
+   * @var \Drupal\Core\Language\LanguageManagerInterface
+   */
+  private LanguageManagerInterface $languageManager;
+
+  /**
+   * The client.
+   *
+   * @var \GuzzleHttp\Client
+   */
+  private Client $client;
+
+  /**
+   * Constructs an ArchiveHelper object.
+   */
+  public function __construct(EntityTypeManagerInterface $entityTypeManager, LanguageManagerInterface $languageManager, Client $client) {
+    $this->entityTypeManager = $entityTypeManager;
+    $this->languageManager = $languageManager;
+    $this->client = $client;
+  }
+
+  /**
+   * Post submission to API.
+   */
+  public function post(array $payload) {
+    try {
+      $submissionId = $payload['submission']['id'] ?? NULL;
+      $handlerConfiguration = $payload['handler']['configuration'] ?? NULL;
+
+      $submission = $this->getSubmission($submissionId);
+      if (NULL === $submission) {
+        throw new ApiHandlerException(sprintf('Cannot load submission %s', $submissionId));
+      }
+
+      $webform = $submission->getWebform();
+
+      $links = [
+        'sender' => $this->generateUrl('<front>'),
+      ];
+      try {
+        $links['get_submission_url'] = $this->generateUrl(
+          'rest.webform_rest_submission.GET',
+          [
+            'webform_id' => $webform->id(),
+            'uuid' => $submission->uuid(),
+          ]
+        );
+      }
+      catch (\Exception $exception) {
+      }
+
+      $data = [
+        'data' => [
+          'webform' => [
+            'id' => $webform->id(),
+          ],
+          'submission' => [
+            'uuid' => $submission->uuid(),
+          ],
+        ],
+        'links' => $links,
+      ];
+
+      if ($handlerConfiguration['post_full_submission'] ?? FALSE) {
+        // @todo Add full submission data.
+        // $data['submission'] += …
+      }
+
+      $apiUrl = $handlerConfiguration['api_url'] ?? NULL;
+      $apiToken = $handlerConfiguration['api_token'] ?? NULL;
+      $this->client->request('POST', $apiUrl, [
+        'headers' => [
+          'Authorization' => sprintf('Token %s', $apiToken),
+        ],
+        RequestOptions::JSON => $data,
+      ]);
+    }
+    catch (\Exception $exception) {
+      throw new ApiHandlerException($exception->getMessage(), $exception->getCode(), $exception);
+    }
+  }
+
+  /**
+   * Get webform submission by id.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   */
+  private function getSubmission(string $id): ?WebformSubmissionInterface {
+    return $this->entityTypeManager
+      ->getStorage('webform_submission')
+      ->load($id);
+  }
+
+  /**
+   * Generate url.
+   */
+  private function generateUrl(string $routeName, array $parameters = [], array $options = []): string {
+    return Url::fromRoute(
+      $routeName,
+      $parameters,
+      $options + [
+        'absolute' => TRUE,
+        'language' => $this->languageManager->getLanguage(LanguageInterface::LANGCODE_NOT_APPLICABLE),
+      ]
+    )->toString();
+  }
+
+}
