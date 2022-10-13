ac38633549188d573fcb3d08ca418c7ab43e4f69
Mikkel Ricky <rimi@aarhus.dk>
2022-10-13 20:37:25 +0200

    Updated forms and flows

--- web/modules/custom/os2forms_selvbetjening/modules/os2forms_selvbetjening_examples/src/Helper.php
+++ web/modules/custom/os2forms_selvbetjening/modules/os2forms_selvbetjening_examples/src/Helper.php
@@ -6,6 +6,7 @@ use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\node\Entity\Node;
 use Drupal\node\NodeStorageInterface;
+use Drupal\taxonomy\TermStorageInterface;
 
 /**
  * A helper class.
@@ -28,6 +29,13 @@ class Helper {
   private NodeStorageInterface $nodeStorage;
 
   /**
+   * The term storage.
+   *
+   * @var \Drupal\taxonomy\TermStorageInterface|\Drupal\Core\Entity\EntityStorageInterface
+   */
+  private TermStorageInterface $termStorage;
+
+  /**
    * The config factory.
    *
    * @var \Drupal\Core\Config\ConfigFactoryInterface
@@ -39,6 +47,7 @@ class Helper {
    */
   public function __construct(EntityTypeManagerInterface $entityTypeManager, ConfigFactoryInterface $configFactory) {
     $this->nodeStorage = $entityTypeManager->getStorage('node');
+    $this->termStorage = $entityTypeManager->getStorage('taxonomy_term');
     $this->configFactory = $configFactory;
   }
 
@@ -50,6 +59,7 @@ class Helper {
     // creating nodes.
     $this->deleteFormNodes();
     $this->createFormNodes();
+    $this->updateAffiliations();
   }
 
   /**
@@ -62,7 +72,7 @@ class Helper {
   /**
    * Create form nodes.
    */
-  private function createFormNodes() {
+  private function createFormNodes(): void {
     foreach ($this->nodeFormIds as $formId) {
       $node = Node::create([
         'type' => 'webform',
@@ -76,17 +86,48 @@ class Helper {
       $node->save();
 
       if ('example_flow_step_2' === $formId) {
-        $config = $this->configFactory->getEditable('maestro.maestro_template.example_flow_page');
-        $config->set('tasks.example_flow_step_2.data.webform_nodes_attached_to', 'node/' . $node->id());
-        $config->save();
+        $this->configFactory->getEditable('maestro.maestro_template.example_flow_page')
+          ->set('tasks.example_flow_step_2.data.webform_nodes_attached_to', 'node/' . $node->id())
+          ->save();
       }
+    }
+  }
+
+  /**
+   * Set affiliation on all flows.
+   */
+  private function updateAffiliations(): void {
+    $terms = $this->termStorage->loadTree('user_affiliation');
+    $affiliations = [];
+    foreach ($terms as $term) {
+      $affiliations[$term->tid] = (string) $term->tid;
+    }
+
+    $formIds = [
+      'example_flow_step_1',
+      'example_flow_step_2',
+    ];
+    foreach ($formIds as $formId) {
+      $this->configFactory->getEditable('webform.webform.' . $formId)
+        ->set('third_party_settings.os2forms_permissions_by_term.settings', $affiliations)
+        ->save();
+    }
+
+    $flowIds = [
+      'example_flow',
+      'example_flow_page',
+    ];
+    foreach ($flowIds as $flowId) {
+      $this->configFactory->getEditable('maestro.maestro_template.' . $flowId)
+        ->set('third_party_settings.os2forms_permissions_by_term.maestro_template_permissions_by_term_settings', $affiliations)
+        ->save();
     }
   }
 
   /**
    * Delete form nodes.
    */
-  private function deleteFormNodes() {
+  private function deleteFormNodes(): void {
     $nodeIds = $this->nodeStorage
       ->getQuery()
       ->condition('webform', $this->nodeFormIds)
