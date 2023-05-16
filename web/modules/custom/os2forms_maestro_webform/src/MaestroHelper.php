06c8f106dbdd292d5c71e31e90222dc30a230390
Mikkel Ricky <rimi@aarhus.dk>
2023-05-16 16:33:08 +0200

    Hmm …

---
+++ web/modules/custom/os2forms_maestro_webform/src/MaestroHelper.php
@@ -0,0 +1,162 @@
+<?php
+
+namespace Drupal\os2forms_maestro_webform;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Config\ImmutableConfig;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\maestro\Engine\MaestroEngine;
+use Drupal\os2forms_maestro_webform\Form\SettingsForm;
+use Drupal\os2forms_maestro_webform\Plugin\WebformHandler\NotificationHandler;
+use Drupal\webform\WebformSubmissionInterface;
+use Drupal\webform\WebformSubmissionStorageInterface;
+use Drupal\webform\WebformTokenManagerInterface;
+
+/**
+ *
+ */
+class MaestroHelper {
+  readonly private ImmutableConfig $config;
+  readonly private WebformSubmissionStorageInterface $webformSubmissionStorage;
+
+  /**
+   *
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entityTypeManager,
+    ConfigFactoryInterface $configFactory,
+    readonly private WebformTokenManagerInterface $tokenManager
+  ) {
+    $this->config = $configFactory->get(SettingsForm::SETTINGS);
+    $this->webformSubmissionStorage = $entityTypeManager->getStorage('webform_submission');
+  }
+
+  /**
+   *
+   */
+  public function maestroZeroUserNotification($templateMachineName, $taskMachineName, $queueID, $notificationType) {
+    // This only fires with a ZERO user-count on notifications. Use this as you
+    // see fit.
+    if ('assignment' === $notificationType) {
+      $templateTask = MaestroEngine::getTemplateTaskByID($templateMachineName, $taskMachineName);
+      $taskType = $templateTask['tasktype'] ?? NULL;
+      if (in_array($taskType, ['MaestroWebform', 'MaestroWebformInherit'], TRUE)) {
+        if ($processID = MaestroEngine::getProcessIdFromQueueId($queueID) ?: NULL) {
+          $entityIdentifiers = MaestroEngine::getAllEntityIdentifiersForProcess($processID);
+          foreach ($entityIdentifiers as $entityIdentifier) {
+            if ('webform_submission' === ($entityIdentifier['entity_type'] ?? NULL)) {
+              $submission = $this->webformSubmissionStorage->load($entityIdentifier['entity_id']);
+              if ($submission) {
+                $this->handleSubmissionNotification($submission, $templateTask, $queueID);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   */
+  public function maestroCanUserExecuteTaskAlter(bool &$returnValue, int $queueID, int $userID): void {
+
+    // Check if this is an anonymous user and we've been barred access already.
+    if ($userID == 0 && $returnValue === FALSE) {
+      // Load the template task and we'll determine if this has our "special"
+      // assignment to a known "anonymous" role.
+      $templateTask = MaestroEngine::getTemplateTaskByQueueID($queueID);
+      $assignments = explode(',', $templateTask['assigned']);
+
+      $knownAnonymousAssignments = array_map(
+        static fn (string $role) => 'role:fixed:' . $role,
+        array_filter($this->config->get('known_anonymous_roles') ?: [])
+      );
+
+      // DEV NOTE!!!! We do NOTHING to ensure that this is a specific task type
+      // or even that this is in our desired workflows. This routine will run
+      // for each and every task execution test and only if we're anonymous.
+      // This should be streamlined and tightened up to check for more specific
+      // task types or process types... perhaps...
+      // In our very specific use case, we are assigning to a fixed role of
+      // Citizen.
+      // This could be a task config option to denote that regardless of what's in
+      // the assignment, we validate this task as executable one way or another.
+      // @todo Add in your own validation routines
+      foreach ($assignments as $assignment) {
+        if (in_array($assignment, $knownAnonymousAssignments, TRUE)) {
+          // This is our use case. Very rigid for now for prototyping/demo
+          // purposes.
+          $returnValue = TRUE;
+        }
+      }
+    }
+  }
+
+  /**
+   *
+   */
+  private function handleSubmissionNotification(WebformSubmissionInterface $submission, array $templateTask, int $queueID): void {
+    $data = $submission->getData();
+    $webform = $submission->getWebform();
+    $handlers = $webform->getHandlers('os2forms_maestro_webform_notification');
+    foreach ($handlers as $handler) {
+      $settings = $handler->getSettings();
+      $notificationSetting = $settings[NotificationHandler::NOTIFICATION];
+      $recipientElement = $notificationSetting[NotificationHandler::RECIPIENT_ELEMENT] ?? NULL;
+      $recipient = $data[$recipientElement] ?? NULL;
+      if (NULL !== $recipient) {
+        // Lifted from MaestroEngine.
+        $data = [
+          'maestro' => [
+            'task' => $templateTask,
+            'queueID' => $queueID,
+          ],
+        ];
+
+        $subject = $this->tokenManager->replace(
+          $notificationSetting[NotificationHandler::NOTIFICATION_SUBJECT],
+          $submission,
+          $data
+        );
+        $content = $this->tokenManager->replace(
+          $notificationSetting[NotificationHandler::NOTIFICATION_CONTENT],
+          $submission,
+          $data
+        );
+
+        if (isset($templateTask['data']['webform_nodes_attached_to'])) {
+          $search = '[maestro:task:data:webform_nodes_attached_to]';
+          $replace = $templateTask['data']['webform_nodes_attached_to'];
+          $subject = str_replace($search, $replace, $subject);
+          $content = str_replace($search, $replace, $content);
+        }
+
+        if (filter_var($recipient, FILTER_VALIDATE_EMAIL)) {
+          $this->sendNotificationEmail($recipient, $subject, $content);
+        }
+        else {
+          $this->sendNotificationDigitalPost($recipient, $subject, $content);
+        }
+      }
+    }
+  }
+
+  /**
+   * Send notification email.
+   */
+  private function sendNotificationEmail(string $recipient, string $subject, string $content): void {
+    mail($recipient, $subject, $content);
+  }
+
+  /**
+   * Send notification digital post.
+   */
+  private function sendNotificationDigitalPost(string $recipient): void {
+    header('content-type: text/plain');
+    echo var_export(NULL, TRUE);
+    die(__FILE__ . ':' . __LINE__ . ':' . __METHOD__);
+    $hest;
+  }
+
+}
---
+++ web/modules/custom/os2forms_maestro_webform/src/Plugin/WebformHandler/NotificationHandler.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace Drupal\os2forms_maestro_webform\Plugin\WebformHandler;
+
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\os2forms_digital_post\Helper\WebformHelperSF1601;
+use Drupal\webform\Plugin\WebformHandlerBase;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Maestro notification handler.
+ *
+ * @WebformHandler(
+ *   id = "os2forms_maestro_webform_notification",
+ *   label = @Translation("Maestro notification"),
+ *   category = @Translation("Web services"),
+ *   description = @Translation("Sends Meastro notfications to users."),
+ *   cardinality = \Drupal\webform\Plugin\WebformHandlerInterface::CARDINALITY_UNLIMITED,
+ *   results = \Drupal\webform\Plugin\WebformHandlerInterface::RESULTS_IGNORED,
+ *   submission = \Drupal\webform\Plugin\WebformHandlerInterface::SUBMISSION_REQUIRED,
+ * )
+ */
+final class NotificationHandler extends WebformHandlerBase {
+  public const NOTIFICATION = 'notification';
+
+  public const TYPE = 'type';
+  public const SENDER_LABEL = 'sender_label';
+  public const NOTIFICATION_SUBJECT = 'notification_subject';
+  public const NOTIFICATION_CONTENT = 'notification_content';
+  public const RECIPIENT_ELEMENT = 'recipient_element';
+
+  /**
+   * Maximum length of sender label.
+   */
+  private const SENDER_LABEL_MAX_LENGTH = 64;
+
+  /**
+   * Maximum length of header label.
+   */
+  private const NOTIFICATION_SUBJECT_MAX_LENGTH = 128;
+
+  /**
+   * The token manager.
+   *
+   * @var \Drupal\webform\WebformTokenManagerInterface
+   */
+  protected $tokenManager;
+
+  /**
+   * The webform helper.
+   *
+   * @var \Drupal\os2forms_digital_post\WebformHelper
+   */
+  protected $webformHelper;
+
+  /**
+   * The template manager.
+   *
+   * @var \Drupal\os2forms_digital_post\Manager\TemplateManager
+   */
+  protected $templateManager;
+
+  /**
+   * The print service consumer.
+   *
+   * @var \Drupal\os2forms_digital_post\Consumer\PrintServiceConsumer
+   */
+  protected $printServiceConsumer;
+
+  /**
+   * The cpr service.
+   *
+   * @var \Drupal\os2forms_cpr_lookup\Service\CprServiceInterface
+   */
+  protected $cprService;
+
+  /**
+   * The webform helper.
+   *
+   * @var \Drupal\os2forms_digital_post\Helper\WebformHelperSF1601
+   */
+  protected WebformHelperSF1601 $helper;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+
+    $instance->loggerFactory = $container->get('logger.factory');
+    $instance->configFactory = $container->get('config.factory');
+    $instance->renderer = $container->get('renderer');
+    $instance->entityTypeManager = $container->get('entity_type.manager');
+    $instance->conditionsValidator = $container->get('webform_submission.conditions_validator');
+    $instance->tokenManager = $container->get('webform.token_manager');
+    $instance->webformHelper = $container->get('os2forms_digital_post.webform_helper');
+    $instance->templateManager = $container->get('os2forms_digital_post.template_manager');
+    $instance->printServiceConsumer = $container->get('os2forms_digital_post.print_service_consumer');
+    $instance->cprService = $container->get('os2forms_cpr_lookup.service');
+    $instance->helper = $container->get(WebformHelperSF1601::class);
+
+    $instance->setConfiguration($configuration);
+
+    return $instance;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getSummary() {
+    $settings = $this->getSettings();
+
+    return [
+      '#settings' => $settings,
+    ] + parent::getSummary();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $formState) {
+    $form[self::NOTIFICATION] = [
+      '#type' => 'fieldset',
+      '#title' => $this->t('Notification'),
+    ];
+
+    $availableElements = $this->getRecipientElements();
+    $form[self::NOTIFICATION][static::RECIPIENT_ELEMENT] = [
+      '#type' => 'select',
+      '#title' => $this->t('Element that contains the recipient identifier (email, CPR or CVR) of the notification'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration[self::NOTIFICATION][self::RECIPIENT_ELEMENT] ?? NULL,
+      '#options' => $availableElements,
+    ];
+
+    $form[self::NOTIFICATION][self::SENDER_LABEL] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Sender label'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration[self::NOTIFICATION][self::SENDER_LABEL] ?? NULL,
+      '#maxlength' => self::SENDER_LABEL_MAX_LENGTH,
+    ];
+
+    $form[self::NOTIFICATION][self::NOTIFICATION_SUBJECT] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Notification subject'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration[self::NOTIFICATION][self::NOTIFICATION_SUBJECT] ?? NULL,
+      '#maxlength' => self::NOTIFICATION_SUBJECT_MAX_LENGTH,
+    ];
+
+    $form[self::NOTIFICATION][self::NOTIFICATION_CONTENT] = [
+      '#type' => 'textarea',
+      '#title' => $this->t('Notification text'),
+      '#required' => TRUE,
+      '#default_value' => $this->configuration[self::NOTIFICATION][self::NOTIFICATION_CONTENT] ?? NULL,
+    ];
+
+    return $this->setSettingsParents($form);
+  }
+
+  /**
+   * Get recipient elements.
+   */
+  private function getRecipientElements(): array {
+    $elements = $this->getWebform()->getElementsDecodedAndFlattened();
+
+    $elementTypes = [
+      'email',
+      'textfield',
+      'cpr_element',
+      'cpr_value_element',
+      'cvr_element',
+      'cvr_value_element',
+    ];
+    $elements = array_filter(
+      $elements,
+      static function (array $element) use ($elementTypes) {
+        return in_array($element['#type'], $elementTypes, TRUE);
+      }
+    );
+
+    return array_map(static function (array $element) {
+      return $element['#title'];
+    }, $elements);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateConfigurationForm(array &$form, FormStateInterface $formState) {
+    parent::validateConfigurationForm($form, $formState);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $formState) {
+    parent::submitConfigurationForm($form, $formState);
+
+    $this->configuration[self::NOTIFICATION] = $formState->getValue(self::NOTIFICATION);
+  }
+
+}
