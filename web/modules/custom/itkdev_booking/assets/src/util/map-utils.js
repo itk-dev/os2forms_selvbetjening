94ef3cd185ecc7128e7b7506eacbecfc64ebc85b
Jeppe Krogh <kjej@aarhus.dk>
2023-10-26 10:19:03 +0200

    getFeatures can now return locations or resources with placeholder geolocation

--- web/modules/custom/itkdev_booking/assets/src/util/map-utils.js
+++ web/modules/custom/itkdev_booking/assets/src/util/map-utils.js
@@ -18,29 +18,47 @@ export function latlngToUTM(lat, long) {
  * @param {object} resources Resources array
  * @returns {Array} Containing openLayer features and tooltip content
  */
-export function getFeatures(resources) {
+export function getFeatures(resources, useLocations = false) {
   // Loop resources and build coordinates and tooltip content
-  const locations = [];
+  const features = [];
 
-  Object.values(resources).forEach((value) => {
-    if (value.location in locations) {
-      locations[value.location].resource_count += 1;
-    } else {
+  if (useLocations) {
+    Object.values(resources).forEach((value) => {
+      if (value.location in resources) {
+        features[value.location].resource_count += 1;
+      } else {
+        if (value.location === "" || value.geoCoordinates === "" || value.geoCoordinates === null) {
+          return;
+        }
+        const geoCoordinates = value.geoCoordinates.split(",");
+        const utmCoordinates = latlngToUTM(geoCoordinates[0], geoCoordinates[1]);
+
+        features[value.location] = {
+          location: value.locationDisplayName ?? value.location,
+          locationId: value.location,
+          northing: utmCoordinates[0],
+          easting: utmCoordinates[1],
+          resource_count: 1,
+        };
+      }
+    });
+  } else {
+    Object.values(resources).forEach((value) => {
       if (value.location === "" || value.geoCoordinates === "" || value.geoCoordinates === null) {
         return;
       }
-      const geoCoordinates = value.geoCoordinates.split(",");
-      const utmCoordinates = latlngToUTM(geoCoordinates[0], geoCoordinates[1]);
+      const randomNorthing = Math.floor(Math.random() * (554220 - 573220 + 1)) + 573220;
+      const randomEasting = Math.floor(Math.random() * (6199604 - 6229304 + 1)) + 6229304;
 
-      locations[value.location] = {
-        location: value.locationDisplayName ?? value.location,
-        locationId: value.location,
-        northing: utmCoordinates[0],
-        easting: utmCoordinates[1],
-        resource_count: 1,
+      features[value.id] = {
+        resourceName: value.resourceDisplayName ?? value.resourceName,
+        resourceId: value.id,
+        resourceMail: value.resourceMail,
+        northing: randomNorthing,
+        easting: randomEasting,
       };
-    }
-  });
+    });
+  }
 
-  return locations;
+  return features;
 }
