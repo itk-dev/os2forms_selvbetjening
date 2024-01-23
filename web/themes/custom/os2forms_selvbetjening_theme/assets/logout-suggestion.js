9321828575513abcb58e15a25309ffc1afd2b184
jekuaitk <jekua@aarhus.dk>
2024-01-23 09:45:41 +0100

    558: Used vanilla js

--- web/themes/custom/os2forms_selvbetjening_theme/assets/logout-suggestion.js
+++ web/themes/custom/os2forms_selvbetjening_theme/assets/logout-suggestion.js
@@ -1,22 +1,19 @@
-(function ($) {
-  $(document).ready(function () {
-    const elem = document.getElementById('logout_suggestion');
-    let url = elem.getAttribute('href');
-    url = replaceUrlParam(url, 'destination', window.location.pathname)
-    elem.setAttribute('href', url)
+window.addEventListener('load', function () {
+  const elem = document.getElementById('logout_suggestion')
+  let url = elem.getAttribute('href')
+  url = replaceUrlParam(url, 'destination', window.location.pathname)
+  elem.setAttribute('href', url)
+}, false)
 
-    // @see https://stackoverflow.com/a/20420424
-    function replaceUrlParam(url, paramName, paramValue)
-    {
-      if (paramValue == null) {
-        paramValue = '';
-      }
-      var pattern = new RegExp('\\b('+paramName+'=).*?(&|#|$)');
-      if (url.search(pattern)>=0) {
-        return url.replace(pattern,'$1' + paramValue + '$2');
-      }
-      url = url.replace(/[?#]$/,'');
-      return url + (url.indexOf('?')>0 ? '&' : '?') + paramName + '=' + paramValue;
-    }
-  });
-})(jQuery)
+// @see https://stackoverflow.com/a/20420424
+function replaceUrlParam (url, paramName, paramValue) {
+  if (paramValue == null) {
+    paramValue = ''
+  }
+  const pattern = new RegExp('\\b(' + paramName + '=).*?(&|#|$)')
+  if (url.search(pattern) >= 0) {
+    return url.replace(pattern, '$1' + paramValue + '$2')
+  }
+  url = url.replace(/[?#]$/, '')
+  return url + (url.indexOf('?') > 0 ? '&' : '?') + paramName + '=' + paramValue
+}
