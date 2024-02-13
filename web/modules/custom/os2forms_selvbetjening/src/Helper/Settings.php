6d05b0677052a97cb12e7fd35bd7f9e5ff52e8c3
jekuaitk <jekua@aarhus.dk>
2024-02-13 15:13:15 +0100

    #761: Added way to export all webform configurations
    # Conflicts:
    #	web/modules/custom/os2forms_selvbetjening/os2forms_selvbetjening.permissions.yml
    #	web/modules/custom/os2forms_selvbetjening/os2forms_selvbetjening.services.yml
    #	web/modules/custom/os2forms_selvbetjening/src/Form/SettingsForm.php
    #	web/modules/custom/os2forms_selvbetjening/src/Helper/WebformConfigurationExporter.php

---
+++ web/modules/custom/os2forms_selvbetjening/src/Helper/Settings.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace Drupal\os2forms_selvbetjening\Helper;
+
+use Drupal\Core\KeyValueStore\KeyValueFactoryInterface;
+use Drupal\Core\KeyValueStore\KeyValueStoreInterface;
+use Drupal\os2forms_selvbetjening\Exception\InvalidSettingException;
+use Drupal\os2forms_selvbetjening\Form\SettingsForm;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+/**
+ * General settings for os2forms_selvbetjening.
+ */
+final class Settings {
+  /**
+   * The store.
+   *
+   * @var \Drupal\Core\KeyValueStore\KeyValueStoreInterface
+   */
+  private KeyValueStoreInterface $store;
+
+  /**
+   * The key value collection name.
+   *
+   * @var string
+   */
+  private $collection = 'os2forms_selvbetjening.';
+
+  /**
+   * The constructor.
+   */
+  public function __construct(KeyValueFactoryInterface $keyValueFactory) {
+    $this->store = $keyValueFactory->get($this->collection);
+  }
+
+  /**
+   * Get fasit api base url.
+   */
+  public function getWebformExportFilename(): string {
+    return $this->get(SettingsForm::SELVBETJENING_WEBFORM_EXPORT_FILENAME, '');
+  }
+
+  /**
+   * Get a setting value.
+   *
+   * @param string $key
+   *   The key.
+   * @param mixed|null $default
+   *   The default value.
+   *
+   * @return mixed
+   *   The setting value.
+   */
+  private function get(string $key, $default = NULL) {
+    $resolver = $this->getSettingsResolver();
+    if (!$resolver->isDefined($key)) {
+      throw new InvalidSettingException(sprintf('Setting %s is not defined', $key));
+    }
+
+    return $this->store->get($key, $default);
+  }
+
+  /**
+   * Set settings.
+   *
+   * @throws \Symfony\Component\OptionsResolver\Exception\ExceptionInterface
+   *
+   * @phpstan-param array<string, mixed> $settings
+   */
+  public function setSettings(array $settings): self {
+    $settings = $this->getSettingsResolver()->resolve($settings);
+    foreach ($settings as $key => $value) {
+      $this->store->set($key, $value);
+    }
+
+    return $this;
+  }
+
+  /**
+   * Get settings resolver.
+   */
+  private function getSettingsResolver(): OptionsResolver {
+    return (new OptionsResolver())
+      ->setDefaults([
+        SettingsForm::SELVBETJENING_WEBFORM_EXPORT_FILENAME => '',
+      ]);
+  }
+
+}
