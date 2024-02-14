ba3f74c41f23cd17adacb6714277495589f9291d
jekuaitk <jekua@aarhus.dk>
2024-02-14 12:18:58 +0100

    #761: Removed unnecessary saved settings

--- web/modules/custom/os2forms_selvbetjening/src/Form/SettingsForm.php
+++ web/modules/custom/os2forms_selvbetjening/src/Form/SettingsForm.php
@@ -5,10 +5,8 @@ namespace Drupal\os2forms_selvbetjening\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
-use Drupal\os2forms_selvbetjening\Helper\Settings;
 use Drupal\os2forms_selvbetjening\Helper\WebformConfigurationExporter;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\OptionsResolver\Exception\ExceptionInterface as OptionsResolverException;
 
 /**
  * Organisation settings form.
@@ -16,14 +14,15 @@ use Symfony\Component\OptionsResolver\Exception\ExceptionInterface as OptionsRes
 final class SettingsForm extends FormBase {
   use StringTranslationTrait;
 
-  public const SELVBETJENING_WEBFORM_EXPORT_FILENAME = 'selvbetjening_webform_export_filename';
-  public const SELVBETJENING_WEBFORM_EXPORT_INCLUDE_TEMPLATES = 'selvbetjening_webform_export_include_templates';
-  public const SELVBETJENING_WEBFORM_EXPORT_INCLUDE_ARCHIVED = 'selvbetjening_webform_export_include_archived';
+  private const SELVBETJENING_WEBFORM_EXPORT_FILENAME = 'selvbetjening_webform_export_filename';
+  private const SELVBETJENING_WEBFORM_EXPORT_FILENAME_DEFAULT = 'webform_config.csv';
+  private const SELVBETJENING_WEBFORM_EXPORT_INCLUDE_TEMPLATES = 'selvbetjening_webform_export_include_templates';
+  private const SELVBETJENING_WEBFORM_EXPORT_INCLUDE_ARCHIVED = 'selvbetjening_webform_export_include_archived';
 
   /**
    * Constructor.
    */
-  public function __construct(private readonly Settings $settings, private readonly WebformConfigurationExporter $webformConfigurationExporter) {
+  public function __construct(private readonly WebformConfigurationExporter $webformConfigurationExporter) {
   }
 
   /**
@@ -31,7 +30,6 @@ final class SettingsForm extends FormBase {
    */
   public static function create(ContainerInterface $container): SettingsForm {
     return new static(
-      $container->get(Settings::class),
       $container->get(WebformConfigurationExporter::class)
     );
   }
@@ -51,34 +49,27 @@ final class SettingsForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state): array {
 
-    $webformExportFilename = $this->settings->getWebformExportFilename();
     $form[self::SELVBETJENING_WEBFORM_EXPORT_FILENAME] = [
       '#type' => 'textfield',
       '#title' => $this->t('Filename for webform csv export'),
+      '#description' => $this->t('Should contain .csv, e.g. %default', ['%default' => self::SELVBETJENING_WEBFORM_EXPORT_FILENAME_DEFAULT]),
       '#required' => TRUE,
-      '#default_value' => !empty($webformExportFilename) ? $webformExportFilename : NULL,
+      '#default_value' => self::SELVBETJENING_WEBFORM_EXPORT_FILENAME_DEFAULT,
     ];
 
-    $includeTemplateWebforms = $this->settings->getIncludeTemplateWebforms();
     $form[self::SELVBETJENING_WEBFORM_EXPORT_INCLUDE_TEMPLATES] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Include template webforms'),
-      '#default_value' => $includeTemplateWebforms,
+      '#default_value' => TRUE,
     ];
 
-    $includeArchivedWebforms = $this->settings->getIncludeArchivedWebforms();
     $form[self::SELVBETJENING_WEBFORM_EXPORT_INCLUDE_ARCHIVED] = [
       '#type' => 'checkbox',
       '#title' => $this->t('Include archived webforms'),
-      '#default_value' => $includeArchivedWebforms,
+      '#default_value' => TRUE,
     ];
 
     $form['actions']['#type'] = 'actions';
-
-    $form['actions']['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Save settings'),
-    ];
 
     $form['actions']['exportWebformConfiguration'] = [
       '#type' => 'submit',
@@ -95,39 +86,13 @@ final class SettingsForm extends FormBase {
    * @phpstan-param array<string, mixed> $form
    */
   public function submitForm(array &$form, FormStateInterface $formState): void {
-    $filename = $formState->getValue(self::SELVBETJENING_WEBFORM_EXPORT_FILENAME);
+
+    $filename = (string) $formState->getValue(self::SELVBETJENING_WEBFORM_EXPORT_FILENAME);
     $includeTemplateWebforms = (bool) $formState->getValue(self::SELVBETJENING_WEBFORM_EXPORT_INCLUDE_TEMPLATES);
     $includeArchivedWebforms = (bool) $formState->getValue(self::SELVBETJENING_WEBFORM_EXPORT_INCLUDE_ARCHIVED);
 
-    $triggeringElement = $formState->getTriggeringElement();
-    if ('exportWebformConfiguration' === ($triggeringElement['#name'] ?? NULL)) {
-      $this->exportWebformConfiguration($filename, $includeTemplateWebforms, $includeArchivedWebforms);
-      return;
-    }
-
-    try {
-      $settings[self::SELVBETJENING_WEBFORM_EXPORT_FILENAME] = $filename;
-      $settings[self::SELVBETJENING_WEBFORM_EXPORT_INCLUDE_TEMPLATES] = $includeTemplateWebforms;
-      $settings[self::SELVBETJENING_WEBFORM_EXPORT_INCLUDE_ARCHIVED] = $includeArchivedWebforms;
-
-      $this->settings->setSettings($settings);
-      $this->messenger()->addStatus($this->t('Settings saved'));
-    }
-    catch (OptionsResolverException $exception) {
-      $this->messenger()->addError($this->t('Settings not saved (@message)', ['@message' => $exception->getMessage()]));
-    }
-
-    $this->messenger()->addStatus($this->t('Settings saved'));
-
-  }
-
-  /**
-   * Export webform configuration.
-   */
-  private function exportWebformConfiguration(string $filename, bool $includeTemplateWebforms, bool $includeArchivedWebforms): void {
     try {
       $this->webformConfigurationExporter->extractWebformConfiguration($filename, $includeTemplateWebforms, $includeArchivedWebforms);
-      $this->messenger()->addStatus($this->t('Data successfully exported'));
     }
     catch (\Throwable $throwable) {
       $message = $this->t('Error exporting webform configuration: %message', ['%message' => $throwable->getMessage()]);
