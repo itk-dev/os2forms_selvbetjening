6130ed26a56f0dc2b44186a6cae61fa15af38af4
Jeppe Kuhlmann Andersen <78410897+jekuaitk@users.noreply.github.com>
2025-08-25 13:29:06 +0200

    Update web/modules/custom/os2forms_selvbetjening/js/dawa_address_autocomplete.js
    Co-authored-by: Mikkel Ricky <rimi@aarhus.dk>

--- web/modules/custom/os2forms_selvbetjening/js/dawa_address_autocomplete.js
+++ web/modules/custom/os2forms_selvbetjening/js/dawa_address_autocomplete.js
@@ -3,7 +3,7 @@
   // Danish addresses contain commas. Original splitValues() method
   // splits upon commas. Combined with default extractLastTerm this results in
   // unexpected behavior when using autocomplete with respect to danish addresses.
-  // Inspired by @https://www.drupal.org/project/drupal/issues/2821181#comment-12012538.
+  // Inspired by https://www.drupal.org/project/drupal/issues/2821181#comment-12012538.
   Drupal.autocomplete.splitValues = function (value) {
     return [ value.trim() ];
   }
